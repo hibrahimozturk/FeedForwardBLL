6b57205e7a8b8a66b41f19e9490e7a9c77215012
Halil İbrahim Öztürk <hibrahimozturk95@gmail.com>
2019-06-21 13:18:22 +0000

    Experiment adam/shallow2-3/all-bn-lrelu

--- source/bll_model.py
+++ source/bll_model.py
@@ -8,7 +8,7 @@ class BLLModel(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.relu    = nn.ReLU(inplace=True)
+        self.lrelu    = nn.LeakyReLU()
 
         self.linear_1_1 = nn.Linear(300, 256)
         self.bn1_1 = nn.BatchNorm1d(256)
@@ -36,16 +36,16 @@ class BLLModel(nn.Module):
     def forward(self, source_vector, target_vector):
         
         
-        branch_1 = self.bn1_1(self.relu(self.linear_1_1(source_vector)))
-        branch_1 = self.relu( self.linear_1_2(branch_1))
+        branch_1 = self.bn1_1(self.lrelu(self.linear_1_1(source_vector)))
+        branch_1 = self.lrelu( self.linear_1_2(branch_1))
         
-        branch_2 = self.bn2_1(self.relu(self.linear_2_1(target_vector)))
-        branch_2 = self.relu(self.linear_2_2(branch_2))     
+        branch_2 = self.bn2_1(self.lrelu(self.linear_2_1(target_vector)))
+        branch_2 = self.lrelu(self.linear_2_2(branch_2))     
         
         x =  self.bn2(torch.cat((branch_1, branch_2), dim=1))
         
-        x =  self.bn3( self.relu(self.fc1(x) ))
-        x =  self.bn4( self.relu( self.fc2(x) ))
+        x =  self.bn3( self.lrelu(self.fc1(x) ))
+        x =  self.bn4( self.lrelu( self.fc2(x) ))
         x =  self.sigmoid( self.fc3(x) )
         
         return x
